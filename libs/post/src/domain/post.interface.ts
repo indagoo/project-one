feb14daa2c91db4a86ddea39d3773277dfad3a98
Ivan Ivanov <indagoo@gmail.com>
2024-01-24 15:36:10 +0500

    ADD: post domain

---
+++ libs/post/src/domain/post.interface.ts
@@ -0,0 +1,22 @@
+export interface IPost {
+  /** Description **/
+  id: string;
+
+  /** Description **/
+  title: string;
+
+  /** Description **/
+  message: string;
+
+  /** Description **/
+  authorId: string;
+
+  /** Description **/
+  published: boolean;
+
+  /** Description **/
+  createdAt: string;
+
+  /** Description **/
+  updatedAt: string;
+}
