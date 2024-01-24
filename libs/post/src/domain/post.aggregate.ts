d9856f9462e4cb44c6c3fde9652b3ee468bb47d3
Ivan Ivanov <indagoo@gmail.com>
2024-01-24 16:47:22 +0500

    ADD: Post aggregate

--- libs/post/src/domain/post.aggregate.ts
+++ libs/post/src/domain/post.aggregate.ts
@@ -1,8 +1,9 @@
-import { IPost } from './post.interface';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
-import { AggregateRoot } from '@nestjs/cqrs';
+import { IPost } from './post.interface';
+import { PostServices } from './services';
 
-export class PostAggregate extends AggregateRoot implements IPost {
+export class PostAggregate extends PostServices implements IPost {
+  @IsUUID()
   id: string = randomStringGenerator();
   title: string;
   message: string;
