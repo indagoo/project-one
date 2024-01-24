820e4fcd3addbc4a318dd67dde06a574081775ec
Ivan Ivanov <indagoo@gmail.com>
2024-01-24 16:56:43 +0500

    ADD: post validators

--- libs/post/src/domain/post.aggregate.ts
+++ libs/post/src/domain/post.aggregate.ts
@@ -1,15 +1,38 @@
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  validateSync,
+} from 'class-validator';
 import { IPost } from './post.interface';
 import { PostServices } from './services';
+import { Exclude } from 'class-transformer';
 
 export class PostAggregate extends PostServices implements IPost {
   @IsUUID()
   id: string = randomStringGenerator();
+
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   message: string;
+
+  @IsUUID()
   authorId: string;
+
+  @IsBoolean()
+  @Exclude()
   published = false;
+
+  @IsString()
   createdAt = new Date().toISOString();
+
+  @IsString()
   updatedAt = new Date().toISOString();
 
   private constructor() {
@@ -19,6 +42,11 @@ export class PostAggregate extends PostServices implements IPost {
   static create(post: Partial<IPost>) {
     const _post = new PostAggregate();
     Object.assign(_post, post);
+    _post.updatedAt = post?.id ? new Date().toISOString() : _post.updatedAt;
+    const errors = validateSync(_post, { whitelist: true });
+    if (!!errors.length) {
+      throw new Error('Post not valid');
+    }
     return _post;
   }
 }
